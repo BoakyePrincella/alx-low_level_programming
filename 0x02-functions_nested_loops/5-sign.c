75856865bbbebb82a33b2349283f1d569c985a21
BoakyePrincella <princellaboakye2003@gmail.com>
2023-12-19 21:59:53 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n:number to be chcked
+ * Return:1 for positive -1 for negative and zero for anyhting else
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(98);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}

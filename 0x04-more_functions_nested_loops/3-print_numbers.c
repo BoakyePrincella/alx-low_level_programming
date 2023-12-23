ab7d9061ac888fc33ca0dfea0c5317f5d545ab48
BoakyePrincella <princellaboakye2003@gmail.com>
2023-12-22 21:47:04 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return:void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+}

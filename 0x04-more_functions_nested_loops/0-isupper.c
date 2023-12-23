616751616e2e8f0bbdc1c4df74f451a45e0b330b
BoakyePrincella <princellaboakye2003@gmail.com>
2023-12-22 21:09:02 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the uppercase character
+ * @c:function parameter
+ * Return:1 for uppercase
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}

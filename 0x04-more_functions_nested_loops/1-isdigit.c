a4b2caeb034a83b5668d12d0e53a9d8f9b14945d
BoakyePrincella <princellaboakye2003@gmail.com>
2023-12-22 21:17:10 -0800

    1-isdigit.c'

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - cheacks for a digit
+ * @c:function parameter
+ * Return:1 for success
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}

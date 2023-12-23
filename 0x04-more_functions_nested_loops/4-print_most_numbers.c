ada2c84665b5a2403199f914ce4446859d38b0d2
BoakyePrincella <princellaboakye2003@gmail.com>
2023-12-22 21:57:26 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return:void
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && 1 != 4)
+putchar(i + '0');
+}
+putchar('\n');
+}

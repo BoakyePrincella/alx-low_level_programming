ff49a5e5afedd7134afca4c6c285c463b1886b01
BoakyePrincella <boakyeprincella2003@gmail.com>
2025-05-15 08:58:57 +0000

    C nested

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * print_line - Check description
+ * @n: input character
+ * Description: Drawsa straight line in the terminal
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	char line = '_';
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			putchar(line);
+	}
+	putchar('\n');
+}

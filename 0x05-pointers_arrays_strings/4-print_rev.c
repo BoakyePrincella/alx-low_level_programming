906bb8208b7e9fd83c292b95d22e7d1ee5eec34c
BoakyePrincella <boakyeprincella2003@gmail.com>
2025-05-20 08:25:48 +0000

    Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - check description
+ * Description: Prints a string in reverse, followed by a new line
+ * @s: input character
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (len != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}
